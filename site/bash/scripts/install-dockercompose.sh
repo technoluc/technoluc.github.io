08bba1c6521e8213f4d9c0f99b587ec6d94cb436
technoluc <luc@technoluc.nl>
2022-06-09 14:17:49 +0200

    delete site

--- site/bash/scripts/install-dockercompose.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest|grep tag_name | cut -d '"' -f 4|sed 's/v//g')
-
-sudo curl -L "https://github.com/docker/compose/releases/download/v$VER/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
